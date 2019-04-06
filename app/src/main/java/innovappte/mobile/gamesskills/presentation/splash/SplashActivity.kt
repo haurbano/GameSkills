8ebc78939c3d8afda80f238edb9e963611d676be
haurbano <hamilton.urbano@grability.com>
2019-04-06 16:49:32 -0500

    First commit

---
+++ app/src/main/java/innovappte/mobile/gamesskills/presentation/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package innovappte.mobile.gamesskills.presentation.splash
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import innovappte.mobile.gamesskills.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
