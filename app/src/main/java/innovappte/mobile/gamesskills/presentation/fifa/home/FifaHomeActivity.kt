3287b478a2ca75b84ecb0d492045f3b9f1b859d2
haurbano <hamilton.urbano@grability.com>
2019-04-06 17:30:38 -0500

    First commit

---
+++ app/src/main/java/innovappte/mobile/gamesskills/presentation/fifa/home/FifaHomeActivity.kt
@@ -0,0 +1,13 @@
+package innovappte.mobile.gamesskills.presentation.fifa.home
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import innovappte.mobile.gamesskills.R
+
+class FifaHomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fifa_home)
+    }
+}
--- app/src/main/java/innovappte/mobile/gamesskills/presentation/splash/SplashActivity.kt
+++ app/src/main/java/innovappte/mobile/gamesskills/presentation/splash/SplashActivity.kt
@@ -2,12 +2,11 @@ package innovappte.mobile.gamesskills.presentation.splash
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import innovappte.mobile.gamesskills.R
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        Thread.sleep(1000)
     }
 }
